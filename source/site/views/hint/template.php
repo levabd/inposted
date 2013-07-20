6683cd8c8ef9de9cd4dd17734f2fd98187eeee98
Paul Roik <proy@ukr.net>
2013-07-21 00:47:45 +0300

    no message

--- source/site/views/hint/template.php
+++ source/site/views/hint/template.php
@@ -8,7 +8,7 @@
     <div class="modal-header my_modal1">
         <button type="button" class="my_modal2 close" ng-click="close(false)">x</button>
         <h3 id="suggestionsLabel" class="my_modal3">
-            <img src="<?= Yii()->baseUrl ?>/img/logo_icon.png" /> Did you know ?
+            <img src="<?= Yii()->baseUrl ?>/img/logo_icon.png" /> Do you know ?
         </h3>
     </div>
     <div class="modal-body mini_post_ser">
