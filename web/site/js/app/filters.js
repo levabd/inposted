bc6e0d63a8eea44a6e59d9f557da1f86126552fa
Yura Fedoriv <yurko.fedoriv@gmail.com>
2013-08-04 23:33:23 +0300

    * unlimited post length

--- web/site/js/app/filters.js
+++ web/site/js/app/filters.js
@@ -5,11 +5,18 @@
 angular.module('inposted.filters', []).
     filter('cut', [function () {
         return function (string) {
-            var result = string.replace(/^(.{0,250})\s.*/, '$1');
-            if (result.length < string.length) {
-                result += '...';
+            if(string.length > 250){
+                var result = string.replace(/^(.{0,250})\s.*/, '$1');
+
+                //TODO: prevent html link break
+
+                if (result.length < string.length) {
+                    result += '...';
+                }
+
+                return result;
             }
 
-            return result;
+            return string;
         }
     }]);
